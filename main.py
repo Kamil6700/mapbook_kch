ccf023ad3656be58cc9155d6b7983a27ce2c8763
Kamil6700 <kamil.cholewa@student.wat.edu.pl>
2026-04-29 20:36:01 +0200

    wip: simple structure for users list

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('geodezja i geoinfromatyka rządzi111')
+zmienna_1:str='kamil'
+zmienna_2:str='wiktoria'
+zmienna_3:str='elizka'
+zmienna_4:str='filip'
+zmienna_5:str='julia'
+zmienna_6:str='jakub'
+
+print(zmienna_1,zmienna_2)
+print(zmienna_3)
+print(zmienna_4)
+print(zmienna_5)
+print(zmienna_6)
