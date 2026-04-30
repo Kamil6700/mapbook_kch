f4714f43c22f2bb473962c8a4e597be8d6d53a08
Kamil6700 <kamil.cholewa@student.wat.edu.pl>
2026-04-30 13:42:21 +0200

    feat: add update function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mapbook_lib.model import users
-from mapbook_lib.controller import  read_data,add_user,remove_user
+from mapbook_lib.controller import  read_data,add_user,remove_user,update_user
 
 
 
@@ -10,6 +10,7 @@ while True:
     print('1 - wyświetl znajomych')
     print('2 - dodaj znajomego')
     print('3 - usuń znajomego')
+    print('4 - zmodyfikuj dane znajomego')
 
     choose=input('wybierz opcję: ')
     if choose =='0':
@@ -20,4 +21,6 @@ while True:
         add_user(users)
     if choose == '3':
         remove_user(users)
+    if choose == '4':
+        update_user(users)
 
--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -1,7 +1,8 @@
-def read_data(users_data: list)->None:
+def read_data(users_data: list) -> None:
     for user in users_data:
         print(
             f'twoj znajomy {user['username']} z miejscowości {user["location"]} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
+
 
 def add_user(users_data: list) -> None:
     name = input('Podaj imię: ')
@@ -10,9 +11,20 @@ def add_user(users_data: list) -> None:
     usermessage = ['']
     users_data.append({'username': name, 'location': location, 'posts': posts, 'usermessage': usermessage}, )
 
+
 def remove_user(users_data: list) -> None:
     name = input('Podaj imię użytkownika do usunięcia z listy znajomych: ')
 
     for user in users_data:
         if user['username'] == name:
             users_data.remove(user)
+
+
+def update_user(users_data: list) -> None:
+    name = input('Podaj imię użytkownika do zmiany: ')
+
+    for user in users_data:
+        if user['username'] == name:
+            user['username'] = input('Podaj nowe imię: ')
+            user['location'] = input('Podaj nową lokalizację: ')
+            user['posts'] = int(input('Podaj liczbę postów: '))
