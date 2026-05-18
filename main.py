a918f86a16cf47ee2ccbd8d2ca88b82f1d61dff3
Kamil6700 <kamil.cholewa@student.wat.edu.pl>
2026-05-18 20:29:15 +0200

    feat: add location function

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from mapbook_lib.model import users
-from mapbook_lib.controller import  read_data,add_user,remove_user,update_user
-
-
-
+from mapbook_lib.controller import  read_data,add_user,remove_user,update_user,get_mapa
 
 
 while True:
@@ -11,6 +8,7 @@ while True:
     print('2 - dodaj znajomego')
     print('3 - usuń znajomego')
     print('4 - zmodyfikuj dane znajomego')
+    print('5 - wyświetl mapę znajomych')
 
     choose=input('wybierz opcję: ')
     if choose =='0':
@@ -23,4 +21,7 @@ while True:
         remove_user(users)
     if choose == '4':
         update_user(users)
+    if choose == '5':
+        get_mapa(users)
+
 
